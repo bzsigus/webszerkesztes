16b8ff3daccdb4432c99264a792cb274f18ac29f
bzsigus <zsigus.bence12@gmail.com>
2024-03-09 15:31:19 +0100

    darkmode fixed

--- js/nav.js
+++ js/nav.js
@@ -1,38 +1,52 @@
-// A gombra kattintáskor az "active" osztály hozzáadása a ".hamb-navbar-menu"-hoz
+//telefon nav expend
 function toggleMenu() {
     var navbarMenu = document.querySelector('.hamb-navbar-menu');
     navbarMenu.classList.toggle('active');
   }
 
+
 function toggleIcon(){
   const icon = document.querySelector('.icon')
   const icon2 = document.querySelector('.hicon')
+  const menuItems = document.querySelectorAll('.navbar-menu li a');
+  const menuItemshamb = document.querySelectorAll('.hamb-navbar-menu li a')
+  const line = document.querySelector('footer span')
 
+  //fehér téma
   if(icon.classList.contains('fa-moon')){
+    line.style.backgroundColor = 'black'
     icon.classList.remove('fa-moon');
     icon.classList.add('fa-sun');
     document.body.style.backgroundColor = 'black';
     document.body.style.color = 'white';
+    menuItems.forEach(item => {
+      item.style.color = 'white';
+  });
     icon2.classList.remove('fa-moon');
     icon2.classList.add('fa-sun');
     document.body.style.backgroundColor = 'black'; 
-    document.body.style.color = 'white';  
-    console.log("done")
+    document.body.style.color = 'white'; 
+    menuItemshamb.forEach(item => {
+      item.style.color = 'white';
+    }); 
   }else{
+    //fekete téma
     icon.classList.remove('fa-sun');
     icon.classList.add('fa-moon');
     document.body.style.backgroundColor = 'white'; 
     document.body.style.color = 'black';  
+    menuItems.forEach(item => {
+    item.style.color = 'black';
+  });
     icon2.classList.remove('fa-sun');
     icon2.classList.add('fa-moon');
     document.body.style.backgroundColor = 'white';
     document.body.style.color = 'black';   
-
-    console.log("done2")
+    menuItemshamb.forEach(item => {
+      item.style.color = 'black';
+      }); 
   }
-
-
-
+  line.style.backgroundColor = 'white'
 }
     
     
